e1c1e953c2a36f80e3017324185bfad33bb52ed4
Lee Martin <hello@leejohnmartin.co.uk>
2023-07-08 22:13:52 +0100

    added: Exit compass

---
+++ Assets/Scripts/Player/PlayerCompass.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCompass : MonoBehaviour
+{
+	public GameObject ExitCompass;
+	public GameObject ExitCompassTail;
+
+	public float CompassDistance = 0.5f;
+
+	private GameObject _exit;
+	// Use this for initialization
+	void Start()
+	{
+		_exit = GameObject.FindWithTag("ExitSign");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (_exit)
+		{
+			PointAtExit();
+		}
+	}
+
+	private void PointAtExit()
+	{
+		Debug.Log("Exit found");
+		Vector3 directionToExit = _exit.transform.position - transform.position;
+        directionToExit = directionToExit.normalized;
+
+		ExitCompass.transform.position = transform.position + (directionToExit * CompassDistance);
+        ExitCompassTail.transform.position = transform.position + (directionToExit * (CompassDistance - 0.2f));
+        Debug.Log(directionToExit);
+	}
+}
+
