7d03bc755dfa66c8664f260c17d271455099e667
Lee Martin <hello@leejohnmartin.co.uk>
2023-07-08 15:52:27 +0100

    added: Gameover text

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -7,6 +7,7 @@ public class UI : MonoBehaviour
 {
     private TextMeshProUGUI _scoreText;
     private TextMeshProUGUI _speedText;
+    private TextMeshProUGUI _gameOverText;
 
     private PlayerStatus _playerStatus;
     private PlayerControl _playerControl;
@@ -14,6 +15,7 @@ public class UI : MonoBehaviour
 
     public GameObject ScoreText;
     public GameObject SpeedText;
+    public GameObject GameEndText;
 
 
     // Start is called before the first frame update
@@ -21,6 +23,7 @@ public class UI : MonoBehaviour
     {
         _scoreText = ScoreText.GetComponent<TextMeshProUGUI>();
         _speedText = SpeedText.GetComponent<TextMeshProUGUI>();
+        _gameOverText = GameEndText.GetComponent<TextMeshProUGUI>();
         var player = GameObject.FindWithTag("Player");
         if (player != null)
         {
@@ -36,12 +39,24 @@ public class UI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _scoreText.SetText($"Score: {_playerStatus.Points}");
-        var (currentSpeed, maxSpeed) = _playerControl.GetSpeedData();
-        if (currentSpeed < 0.01f)
+        if (_playerStatus.IsPlaying)
         {
-            currentSpeed = 0;
+            _speedText.enabled = true;
+            _gameOverText.enabled = false;
+            _scoreText.SetText($"Score: {_playerStatus.Points}");
+            var (currentSpeed, maxSpeed) = _playerControl.GetSpeedData();
+            if (currentSpeed < 0.01f)
+            {
+                currentSpeed = 0;
+            }
+            _speedText.SetText($"Speed: {currentSpeed:0.00}/{maxSpeed}");
         }
-        _speedText.SetText($"Speed: {currentSpeed:0.00}/{maxSpeed}");
+        else
+        {
+            _speedText.enabled = false;
+            _gameOverText.enabled = true;
+            var closingText = _playerStatus.GameStatus == GameStatus.Won ? "You Win!" : "Game Over";
+            _gameOverText.SetText($"{closingText}\nPress \"R\" to Restart");
+        }
     }
 }
