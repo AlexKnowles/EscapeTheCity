f87b07e22793c378ca4a5187519dc3c5372da697
Alex Knowles <git@alexknowles.info>
2023-07-09 14:59:30 +0100

    Add some ui stuff

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
     private TextMeshProUGUI _scoreText;
     private TextMeshProUGUI _speedText;
     private TextMeshProUGUI _gameOverText;
+    private Slider _timeRemainingSlider;
+    private Image _timeRemainingFillImage;
+    private Image _timeRemainingBackgroundImage;
     private TextMeshProUGUI _timeRemainingText;
 
     private PlayerStatus _playerStatus;
@@ -17,6 +21,7 @@ public class UI : MonoBehaviour
     public GameObject ScoreText;
     public GameObject SpeedText;
     public GameObject GameEndText;
+    public GameObject TimeRemainingSlider;
     public GameObject TimeRemainingText;
 
 
@@ -26,12 +31,19 @@ public class UI : MonoBehaviour
         _scoreText = ScoreText.GetComponent<TextMeshProUGUI>();
         _speedText = SpeedText.GetComponent<TextMeshProUGUI>();
         _gameOverText = GameEndText.GetComponent<TextMeshProUGUI>();
+
         _timeRemainingText = TimeRemainingText.GetComponent<TextMeshProUGUI>();
+        _timeRemainingSlider = TimeRemainingSlider.GetComponent<Slider>();
+        _timeRemainingFillImage = _timeRemainingSlider.transform.Find("Fill Area").Find("Fill").GetComponent<Image>();
+        _timeRemainingBackgroundImage = _timeRemainingSlider.transform.Find("Background").GetComponent<Image>();
+
         var player = GameObject.FindWithTag("Player");
         if (player != null)
         {
             _playerStatus = player.GetComponent<PlayerStatus>();
             _playerControl = player.GetComponent<PlayerControl>();
+
+            _timeRemainingSlider.maxValue = _playerStatus.Lifetime;
         }
         else
         {
@@ -52,8 +64,8 @@ public class UI : MonoBehaviour
             {
                 currentSpeed = 0;
             }
-            _speedText.SetText($"Speed: {currentSpeed:0.00}/{maxSpeed}");
-            _timeRemainingText.SetText($"Time Left: {_playerStatus.TimeRemaining:00.00}");
+            _speedText.SetText($"Speed: \n {currentSpeed:0.00}m/s");
+            UpdateTimeRemainingSlider();
 
             if (_playerStatus.PlayerShouldUnwreckThemselves)
             {
@@ -72,6 +84,31 @@ public class UI : MonoBehaviour
             _gameOverText.enabled = true;
             var closingText = _playerStatus.GameStatus == GameStatus.Won ? "You Win!" : "Game Over";
             _gameOverText.SetText($"{closingText}\nPress \"R\" to Restart");
+        }
+    }
+
+    private void UpdateTimeRemainingSlider()
+    {
+        _timeRemainingText.SetText($"{_playerStatus.TimeRemaining:00.00}");
+
+        _timeRemainingSlider.value = _playerStatus.TimeRemaining;
+
+        float timeRemainingAsPercent = (_timeRemainingSlider.value / _timeRemainingSlider.maxValue); 
+
+        if (timeRemainingAsPercent >= 0.65f)
+        {
+            _timeRemainingFillImage.color = new(0.2745098f, 1f, 0);
+            _timeRemainingBackgroundImage.color = new(0.2431373f, 0.7372549f, 0.2392157f);
+        }
+        else if (timeRemainingAsPercent >= 0.30f)
+        {
+            _timeRemainingFillImage.color = new(1f, 0.7450981f, 0f);
+            _timeRemainingBackgroundImage.color = new(0.7372549f, 0.6f, 0.2392157f);
+        }
+        else
+        {
+            _timeRemainingFillImage.color = new(1f, 0f, 0.3227463f);
+            _timeRemainingBackgroundImage.color = new(0.7372549f, 0.2392157f, 0.3281927f);
         }
     }
 }
